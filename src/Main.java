c9133a50e36137acf60d523bc900f7253db1b5ba
GUIBERT Vincent <vincent.guibert@externe.maif.fr>
2026-02-25 10:12:18 +0100

    feat: ajout de la ferme + refacto building

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import models.*;
 public class Main {
     public static void main(String[] args) {
 
-        new GameLoop().gameLoop();
         System.out.println("Bienvenu sur JEmpire V2\n\n");
+        new GameLoop().gameLoop();
+
     }
 }
