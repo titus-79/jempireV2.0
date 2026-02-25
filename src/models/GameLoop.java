23a8273956b3dfa2faa2bef7b89d02c2cfec90f6
GUIBERT Vincent <vincent.guibert@externe.maif.fr>
2026-02-25 16:15:09 +0100

    Menu des batiments fonctionnel en tour direct

--- src/models/GameLoop.java
+++ src/models/GameLoop.java
@@ -1,5 +1,6 @@
 package models;
 
+import models.building.Building;
 import models.building.House;
 import models.units.Villagers;
 
@@ -13,32 +14,31 @@ public class GameLoop {
 
     private int loopOfGame;
     private Resources resources;
-    private boolean victory = false;
-    private boolean defeat = false;
     private Scanner myScan = new Scanner(System.in);
     private List<Villagers> villagers;
-    private List<House> houses;
+    private List<Building> villages;
 
     public GameLoop() {
-        this.resources = new Resources(15, 10, 10, 0, 10);
+        this.resources = new Resources(25, 10, 10, 0, 10);
         this.villagers = new ArrayList<>();
         villagers.add(new Villagers());
-        this.houses = new ArrayList<>();
-        houses.add(new House(resources));
+        this.villages = new ArrayList<>();
+        villages.add(new House(resources));
     }
 
     public void gameLoop() {
         int turn = 1;
-        while (!victory || !defeat) {
+        boolean finish = false;
+        while (!finish){
             System.out.println("Tour " + turn);
-            GameView.menu(myScan, resources, villagers, houses);
+            finish = GameView.menu(myScan, resources, villagers, villages);
             turn++;
         }
     }
 
     public void buildHouse() {
         if (resources.getWood() >= 10) {
-            houses.add(new House(resources));
+            villages.add(new House(resources));
         } else {
             System.out.println("Pas assez de bois !");
         }
